01b50d03bd3c1c55ab741f8f68235bc8a1d7fa00
Fer4321-afk <bardisa@colegiomiralmonte.es>
2025-10-29 15:39:28 +0100

    Areglo de ultimo ejercicio , subida de todos los archivos x comandos

---
+++ Tareas/tarea1.js
@@ -0,0 +1,17 @@
+// Pedir números al usuario
+let num1 = parseFloat(prompt("Introduce el primer número:"));
+let num2 = parseFloat(prompt("Introduce el segundo número:"));
+
+// Calcular la suma
+let suma = num1 + num2;
+
+// Mostrar el resultado en un alert
+alert(
+  "El resultado de sumar " +
+    num1 +
+    " y " +
+    num2 +
+    " es " +
+    suma +
+    "\nGracias por usar este programa."
+);
